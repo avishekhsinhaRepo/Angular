1b08ba6049e935b2bb0ab343865ae14631417d8d
Avishekh Sinha <avishekh_sinha@optum.com>
2021-08-17 15:03:07 +0530

    Routing the Modules

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
+
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -16,8 +15,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
-    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
